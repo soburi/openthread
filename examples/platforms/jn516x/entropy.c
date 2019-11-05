e93dcf35037dc7dfa7572bde97f153722012db8b
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2019-11-06 01:06:24 +0900

    entropy

--- examples/platforms/jn516x/entropy.c
+++ examples/platforms/jn516x/entropy.c
@@ -1,21 +1,78 @@
+/*
+ *  Copyright (c) 2019, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file
+ *   This file implements an entropy source based on TRNG.
+ *
+ */
+
 #include <openthread/platform/entropy.h>
+
 #include <openthread-core-config.h>
 #include <openthread/config.h>
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <utils/code_utils.h>
 
 #include "platform-jn516x.h"
 
 void jn516xRandomInit(void)
 {
+    vAHI_StartRandomNumberGenerator(E_AHI_RND_CONTINUOUS,
+                                    E_AHI_INTS_DISABLED);
 }
 
 void jn516xRandomDeinit(void)
 {
+    vAHI_StopRandomNumberGenerator();
 }
 
 otError otPlatEntropyGet(uint8_t *aOutput, uint16_t aOutputLength)
 {
-	(void)aOutput;
-	(void)aOutputLength;
-	return 0;
+    otError  error = OT_ERROR_NONE;
+
+    uint16_t index = 0;
+
+    do
+    {
+        while(!bAHI_RndNumPoll() ) { }
+        uint16_t rnd = u16AHI_ReadRandomNumber();
+        aOutput[index++] = (&rnd)[0];
+        if(index < aOutputLength) {
+            aOutput[index++] = (&rnd)[1];
+        }
+    } while (index < aOutputLength);
+
+
+    return error;
 }
