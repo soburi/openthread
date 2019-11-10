fdb59c65d34354db4eb4f8214c9087bb0c5c83c4
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2019-11-10 21:38:56 +0900

    system

--- examples/platforms/jn516x/system.c
+++ examples/platforms/jn516x/system.c
@@ -34,28 +34,125 @@
 
 #include <openthread-core-config.h>
 #include <openthread/config.h>
+
 #include <openthread/platform/logging.h>
+
 #include "openthread-system.h"
+#include "platform-jn516x.h"
+
+#include <dbg_uart.h>
+
 #include <openthread/config.h>
 #include <openthread/platform/toolchain.h>
 
+extern int main(int argc, char* argv[]);
+void AppColdStart(void);
+void AppWarmStart(void);
+void __cxa_pure_virtual(void);
+
+extern bool gPlatformPseudoResetWasRequested;
+
+void __cxa_pure_virtual(void)
+{
+    while (1)
+        ;
+}
+
+extern int main(int argc, char* argv[]);
+
+__WEAK void AppColdStart()
+{
+    vAHI_WatchdogStop();
+    (void)u32AHI_Init();
+    char* argv[] = { "ColdStart" };
+    main(1, argv );
+}
+
+__WEAK void AppWarmStart()
+{
+}
+
 void otSysInit(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+    OT_UNUSED_VARIABLE(argc);
+    OT_UNUSED_VARIABLE(argv);
+
+    if (gPlatformPseudoResetWasRequested)
+    {
+        otSysDeinit();
+    }
+
+#if (OPENTHREAD_CONFIG_LOG_OUTPUT == OPENTHREAD_CONFIG_LOG_OUTPUT_PLATFORM_DEFINED) || \
+    (OPENTHREAD_CONFIG_LOG_OUTPUT == OPENTHREAD_CONFIG_LOG_OUTPUT_NCP_SPINEL)
+    jn516xLogInit();
+#endif
+    jn516xAlarmInit();
+    jn516xRandomInit();
+    if (!gPlatformPseudoResetWasRequested)
+    {
+#if ((UART_AS_SERIAL_TRANSPORT == 1) || (USB_CDC_AS_SERIAL_TRANSPORT == 1))
+        jn516xUartInit();
+#endif
+    }
+    else
+    {
+#if ((UART_AS_SERIAL_TRANSPORT == 1) || (USB_CDC_AS_SERIAL_TRANSPORT == 1))
+        jn516xUartClearPendingData();
+#endif
+    }
+
+#if (SPIS_AS_SERIAL_TRANSPORT == 1)
+    jn516xSpiSlaveInit();
+#endif
+    jn516xMiscInit();
+    jn516xRadioInit();
+    //jn516xTempInit();
+
+
+    gPlatformPseudoResetWasRequested = false;
 }
+
 void otSysDeinit(void)
 {
+    //jn516xTempDeinit();
+    jn516xRadioDeinit();
+    jn516xMiscDeinit();
+#if (SPIS_AS_SERIAL_TRANSPORT == 1)
+    jn516xSpiSlaveDeinit();
+#endif
+    if (!gPlatformPseudoResetWasRequested)
+    {
+#if ((UART_AS_SERIAL_TRANSPORT == 1) || (USB_CDC_AS_SERIAL_TRANSPORT == 1))
+        jn516xUartDeinit();
+#endif
+    }
+    jn516xRandomDeinit();
+    jn516xAlarmDeinit();
+#if (OPENTHREAD_CONFIG_LOG_OUTPUT == OPENTHREAD_CONFIG_LOG_OUTPUT_PLATFORM_DEFINED) || \
+    (OPENTHREAD_CONFIG_LOG_OUTPUT == OPENTHREAD_CONFIG_LOG_OUTPUT_NCP_SPINEL)
+    jn516xLogDeinit();
+#endif
 }
+
 bool otSysPseudoResetWasRequested(void)
 {
-	return 0;
+    return gPlatformPseudoResetWasRequested;
 }
+
 void otSysProcessDrivers(otInstance *aInstance)
 {
-	(void)aInstance;
+    jn516xRadioProcess(aInstance);
+#if ((UART_AS_SERIAL_TRANSPORT == 1) || (USB_CDC_AS_SERIAL_TRANSPORT == 1))
+    jn516xUartProcess();
+#endif
+#if (SPIS_AS_SERIAL_TRANSPORT == 1)
+    jn516xSpiSlaveProcess();
+#endif
+    //jn516xTempProcess();
+    jn516xAlarmProcess(aInstance);
 }
-//__WEAK void otSysEventSignalPending(void)
-OT_TOOL_WEAK void otSysEventSignalPending(void)
+
+__WEAK void otSysEventSignalPending(void)
 {
+    // Intentionally empty
 }
